151c5cbe6f96a14aaf29ba7499a3ffc839af7292
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-12-20 17:51:53 +0000

    starting

---
+++ src/main/kotlin/io/github/realyusufismail/kotlinspringbootdemo/KotlinSpringBootDemoApplication.kt
@@ -0,0 +1,11 @@
+package io.github.realyusufismail.kotlinspringbootdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBootDemoApplication>(*args)
+}
