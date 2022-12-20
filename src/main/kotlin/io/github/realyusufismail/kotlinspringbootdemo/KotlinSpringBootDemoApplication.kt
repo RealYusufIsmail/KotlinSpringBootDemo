b8aa7b2cf9cdf78ab811dd3052ca9881c08ba24d
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-12-20 18:04:13 +0000

    The word blog now appears

--- src/main/kotlin/io/github/realyusufismail/kotlinspringbootdemo/KotlinSpringBootDemoApplication.kt
+++ src/main/kotlin/io/github/realyusufismail/kotlinspringbootdemo/KotlinSpringBootDemoApplication.kt
@@ -1,5 +1,6 @@
 package io.github.realyusufismail.kotlinspringbootdemo
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class KotlinSpringBootDemoApplication
 
 fun main(args: Array<String>) {
-    runApplication<KotlinSpringBootDemoApplication>(*args)
+    runApplication<KotlinSpringBootDemoApplication>(*args) {
+        setBannerMode(Banner.Mode.OFF)
+    }
 }
